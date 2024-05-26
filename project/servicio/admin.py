9de3df6f823055885446015481edb9eb8845a9d7
Fernando VAGO GRECCO <vagogreccofer@gmail.com>
2024-05-26 19:45:13 -0300

    Created Models

--- project/servicio/admin.py
+++ project/servicio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ServicioContratado, ServicioOfrecido
+
+admin.site.register(ServicioContratado)
+admin.site.register(ServicioOfrecido)
--- project/servicio/models.py
+++ project/servicio/models.py
@@ -1,3 +1,29 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class ServicioOfrecido(Servicio, models.Model):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    disponible_desde = models.DateField()
+    disponible_hasta = models.DateField()
+    fecha_publicacion = models.DateField(auto_now_add=True)
+
+
+class ServicioContratado(Servicio, models.Model):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+    presupuesto_maximo = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_solicitud = models.DateField(auto_now_add=True)
+    fecha_limite = models.DateField()
